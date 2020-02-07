d970452aa07f05c75346741c11f92867eb73b38c
Paul Linck <paul.linck@gmail.com>
2020-02-06 20:27:58 -0500

    Got login partiall working - firebase updates need fixing

---
+++ JustGroceries/Models/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  JustGroceries
+//
+//  Created by Paul Linck on 2/6/20.
+//  Copyright © 2020 Paul Linck. All rights reserved.
+//
+
+import Foundation
---
+++ JustGroceries/Views/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  JustGroceries
+//
+//  Created by Paul Linck on 2/6/20.
+//  Copyright © 2020 Paul Linck. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
